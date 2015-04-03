f47db50002dae05d7d73b144eb789ddc8252ea00
“Jack <yellowponys@gmail.com>
2015-04-03 19:28:25 +1100

    yay

--- controller.py
+++ controller.py
@@ -1,6 +1,6 @@
-import bottle, sqlite3, bottle_sqlite
-
-install(SQLitePlugin(dbfile='glossary.db'))
+from bottle import route, install, template, error, run
+import sqlite3
+from bottle_sqlite import SQLitePlugin
 
 @route('/static/<filename:path>')
 def send_static(filename):
@@ -8,12 +8,14 @@ def send_static(filename):
 
 @route('/')
 def retrieve_data():
-    db.execute('SELECT * FROM posts WHERE id = ?')
+    return template('home.tpl')
 
 @route('/', method='POST')
 def get_ajax():
     username = request.forms.get('username')
 
-@error('*')
+@error(404)
 def errors(error):
     return template('error.tpl', error=code)
+
+run(host='0.0.0.0', port=8080, debug=True, reloader=True)
